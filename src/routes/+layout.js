af994738a9e15b00e14ea271ffbc605de39649f4
ganbayar13 <ganbayar13@gmail.com>
2023-05-08 15:10:28 +0800

    Update +layout.js

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,16 +1,4 @@
-// import { browser } from '$app/environment';
-// import '$lib/i18n';
-// import { locale, waitLocale } from 'svelte-i18n';
-
 export const prerender = true;
 export const ssr = true;
 export const csr = false;
 export const trailingSlash = 'always';
-
-// /** @type {import('./$types').PageLoad} */
-// export const load = async () => {
-// 	if (browser) {
-// 		locale.set(window.navigator.language);
-// 	}
-// 	await waitLocale();
-// };
