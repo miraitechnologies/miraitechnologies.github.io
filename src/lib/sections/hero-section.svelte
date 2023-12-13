f8a1e61af8ba0ebd012d04e96533fc19310c7544
ganbayar13 <ganbayar13@gmail.com>
2023-12-13 16:16:17 +0800

    Update hero-section.svelte

--- src/lib/sections/hero-section.svelte
+++ src/lib/sections/hero-section.svelte
@@ -7,50 +7,41 @@
 <svelte:window bind:scrollY={y} />
 
 <section id="hero-section" class="relative" use:viewport on:enterViewport={() => (isEnter = true)}>
-	<div
-		class="bg-section bg-section-1 transition-opacity duration-1000 {isEnter
-			? 'opacity-100'
-			: 'opacity-0'}"
-	/>
+	<div class="bg-section bg-section-1" />
 	<div class="bg-shape bg-shape-1" />
-	<div class="container mx-auto">
-		<div class="min-h-screen flex justify-center relative">
-			<div class="grow grid grid-cols-1 lg:grid-cols-2 lg:gap-x-10">
-				<div
-					class="flex opacity-50 lg:mt-0 lg:opacity-100 order-2 -z-10"
+	<div class="min-h-screen grid bg-cover bg-bottom bg-[url('/images/1.png')]">
+		<div
+			class="col-start-1 row-start-1  bg-gradient-to-b from-[#081336] to-[#081336]/0 w-full h-full"
+		/>
+		<div class="col-start-1 row-start-1 mx-auto my-auto">
+			<h1 class="text-6xl lg:text-9xl font-black uppercase text-white text-center px-2">
+				Create the <b
+					class="text-transparent bg-clip-text bg-gradient-to-r from-[#00a0e9] to-[#0068b7]"
+					>future.</b
 				>
-					<div
-						class="lg:my-auto w-full aspect-square lg:rounded-3xl bg-cover"
-						style="background-image: url('/images/1.png')"
-					/>
-				</div>
-				<div
-					class="flex flex-col justify-center lg:justify-center space-y-12 lg:space-y-12 order-1 p-2 lg:p-0 absolute top-0 bottom-0 lg:relative"
-				>
-					<h1 class="text-6xl lg:text-8xl font-black uppercase text-white text-center lg:text-left">
-						Create the <span class="">future</span>.
-					</h1>
-					<h3 class="text-xl font-medium uppercase lg:text-white/80 text-center lg:text-left z-10">
-						We help to make the future with coding that supports people and vehicles.
-					</h3>
-				</div>
-			</div>
-			<div class="self-center py-6 transition-opacity bottom-0 absolute" class:opacity-0={y > 0}>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="animate-bounce w-8 h-8 opacity-50"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5"
-					/>
-				</svg>
-			</div>
+			</h1>
+			<h3 class="text-xl lg:text-3xl font-medium uppercase mt-6 text-center px-2">
+				We help to make the future with coding that supports people and vehicles.
+			</h3>
+		</div>
+		<div
+			class="col-start-1 row-start-1 mx-auto mt-auto p-6 transition-opacity"
+			class:opacity-0={y > 0}
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="animate-bounce w-8 h-8 opacity-50"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5"
+				/>
+			</svg>
 		</div>
 	</div>
 </section>
