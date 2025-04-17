8279fe2840671cff5d76ce954358c6e9f0cb18f8
tuguldur <tuguldur@nashatech.com>
2025-04-17 14:40:54 +0800

    fix responsiveness

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -70,15 +70,27 @@
 		/>
 		<div class="container mx-auto px-5 relative">
 			<div
-				class="flex justify-between items-center h-14 lg:h-20 max-[340px]:flex-col max-[340px]:pt-5 max-[340px]:gap-5"
+				class="flex justify-between items-center h-14 lg:h-20 max-[550px]:flex-col max-[550px]:pt-5 max-[550px]:gap-5"
 			>
-				<div>
+				<div class="flex max-[550px]:justify-between max-[550px]:w-full max-[550px]:min-w-[350px]">
 					<div
 						class="h-8 lg:h-10 flex flex-row items-center"
 						title="Mobile, Innovation, Robotics ,Artificial, Intellence"
 					>
 						<img src="/images/logo.svg" class="h-8 lg:h-10" alt="Logo" />
 						<img src="/images/logo_text.svg" class="h-6 lg:h-8 ml-2" alt="Logo Text" />
+					</div>
+					<div
+						class="items-center gap-2 ml-8 pl-8 border-l border-opacity-20 border-[#ffffff33] opacity-60 hidden  max-[550px]:flex"
+					>
+						<button
+							class="bare-button {selectedValue === 'en-US' ? 'selected-button' : ''}"
+							on:click={() => selectOption('en-US')}>EN</button
+						>
+						<button
+							class="bare-button {selectedValue === 'jp' ? 'selected-button' : ''}"
+							on:click={() => selectOption('jp')}>JP</button
+						>
 					</div>
 				</div>
 				<div class="hidden lg:block">
@@ -114,12 +126,12 @@
 							class="block uppercase font-bold opacity-60 hover:opacity-100 transition-opacity duration-300 relative z-10"
 							href="http://Summit2025.mirai-technologies.com"
 							target="_blank"
-							rel="noreferrer">Summit 2025</a
+							rel="noreferrer">Summit 2025s</a
 						>
 					</li>
 				</ul>
 				<div
-					class="flex items-center gap-2 ml-8 pl-8 border-l border-opacity-20 border-[#ffffff33] opacity-60"
+					class="flex items-center gap-2 ml-8 pl-8 border-l border-opacity-20 border-[#ffffff33] opacity-60 max-[550px]:hidden"
 				>
 					<button
 						class="bare-button {selectedValue === 'en-US' ? 'selected-button' : ''}"
