560dd57757f6cf11596d56fc9ba58c8c98a16329
Tuguldur Bayar <tuguldurbayar@miraitechnologies.com>
2025-11-10 15:14:49 +0800

    add iso section , add menu for iso, add scrolling to it

--- src/lib/section-header.svelte
+++ src/lib/section-header.svelte
@@ -1,9 +1,10 @@
 <script>
 	export let title = '';
 	export let description = '';
+	export let id = '';
 </script>
 
-<h2 class="text-5xl lg:text-7xl uppercase font-black text-white text-center">{title}</h2>
+<h2 id={id} class="text-5xl lg:text-7xl uppercase font-black text-white text-center">{title}</h2>
 {#if description && description.length > 0}
 	<p class="font-extralight text-xl text-white/80 text-center max-w-2xl mx-auto mt-3">
 		{description}
